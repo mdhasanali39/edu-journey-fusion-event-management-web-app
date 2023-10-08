fe0d2658c609f9f411b53758a60060cda98d840d
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-10-08 07:39:56 +0600

    Context-Update: successfully created sign up with email and password, sign in with email and password, sign in with google and logout functions and pass all the functions globaly by AuthContext with value property

--- src/Context/ContextProvider.jsx
+++ src/Context/ContextProvider.jsx
@@ -1,18 +1,40 @@
-
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { createContext } from "react";
-
+import auth from "../Firebase/firebaseConfig";
 
 export const AuthContext = createContext();
 
-
+// creating instance of google provider object
+const provider = new GoogleAuthProvider();
 
 const ContextProvider = ({children}) => {
 
-    
+    // Sign up new users with an email address and password
+    const createUser = (email, password) =>{
+        return createUserWithEmailAndPassword(auth, email,password);
+    }
+
+    // Sign in a user with an email address and password
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // To sign in with a pop-up window
+    const googleSignIn = () =>{
+        return signInWithPopup(auth, provider);
+    }
+
+    // To sign out a user
+    const logOut = () =>{
+        return signOut(auth);
+    }
 
 
     const authData = {
-
+        createUser,
+        signIn,
+        googleSignIn,
+        logOut,
     }
 
     return (
