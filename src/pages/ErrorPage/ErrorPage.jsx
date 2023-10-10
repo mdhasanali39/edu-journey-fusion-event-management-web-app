4a28318274df0b47715b0a43122d53405394954e
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-10-10 10:15:03 +0600

    ErrorPage: updated error page with one not found svg image, one button for go back to home

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
+import notFoundImg from "/page-not-found.svg";
+
 const ErrorPage = () => {
-    return (
-        <div>
-            <h2 className='text-5xl'>Page not found</h2>
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <img className="w-2/4 h-2/4" src={notFoundImg} />
+      <h2 className="text-5xl text-clr-red font-bold">Page not found</h2>
+
+      <Link to={'/'}>
+        <button
+          className="flex select-none items-center gap-2 rounded-lg py-3 px-6 mt-7 text-center align-middle font-bold text-sm uppercase bg-gray-700 text-white transition ease-linear duration-300 border hover:bg-white hover:border-clr-red hover:text-clr-red active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          type="button"
+        >
+          Go Home
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
